97ea0366eedb8d2bfd18cda163a6455ed52ab8cb
Michael Gatuma <50529359+michaelgatuma@users.noreply.github.com>
2023-06-05 16:40:10 +0300

    Update hooks.php

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -11,3 +11,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |	http://codeigniter.com/user_guide/general/hooks.html
 |
 */
+$hook['pre_controller'] = array(
+    'class' => 'DisableMiddleware',
+    'function' => 'run',
+    'filename' => 'DisableMiddleware.php',
+    'filepath' => 'libraries',
+);
